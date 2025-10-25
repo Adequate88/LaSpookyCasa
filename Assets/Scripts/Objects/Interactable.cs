e8cb84370ddd8ab27446c0e694540d10ec694149
Adequate88 <saloalessandro@gmail.com>
2025-10-25 10:22:07 +0200

    Random differences appear and tracking

--- Assets/Scripts/Objects/Interactable.cs
+++ Assets/Scripts/Objects/Interactable.cs
@@ -5,6 +5,7 @@ public class Interactable : MonoBehaviour
 {
     public TextMeshProUGUI interactText;
     public GameObject imageToShow;
+    private GameObject canvasGO;
 
     [SerializeField] AudioSource sfxEnter;
     [SerializeField] AudioSource sfxExit;
@@ -12,10 +13,21 @@ public class Interactable : MonoBehaviour
 
     private bool playerOnRange;
 
+    void Awake()
+    {
+        // find the nearest Canvas above imageToShow
+        if (imageToShow != null)
+        {
+            var canvas = imageToShow.GetComponentInParent<Canvas>(true);
+            if (canvas != null) canvasGO = canvas.gameObject;
+        }
+    }
+
     void Start()
     {
         interactText.gameObject.SetActive(false); // Hide text at start
         imageToShow.SetActive(false); // Hide image at start
+        canvasGO.SetActive(false); // Hide Canvas entirely
     }
 
     void Update()
@@ -30,6 +42,7 @@ public class Interactable : MonoBehaviour
             {
                 sfxExit.Play();
             }
+            canvasGO.SetActive(!canvasGO.activeSelf);
             imageToShow.SetActive(!imageToShow.activeSelf);
         }
     }
@@ -51,6 +64,7 @@ public class Interactable : MonoBehaviour
             interactText.gameObject.SetActive(false);
             playerOnRange = false;
             imageToShow.SetActive(false);
+            canvasGO.SetActive(false);
         }
     }
 }
