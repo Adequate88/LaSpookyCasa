d291e8dc421b6e16ebc14a6e7e8470ccdfe44546
Adequate88 <saloalessandro@gmail.com>
2025-10-25 12:06:12 +0200

    Objects change color when in range + Gray background when viewing images

--- Assets/Scripts/Objects/transitionToNight.cs
+++ Assets/Scripts/Objects/transitionToNight.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 public class transitionToNight : MonoBehaviour
 {
 
+    [SerializeField] GameObject fadeHandler;
     private bool playerOnRange;
 
     void Start()
     {
-
+        fadeHandler.SetActive(false);
     }
 
     void Update()
     {
         if (playerOnRange && Input.GetKeyDown(KeyCode.E))
-        {
+        {   
+            fadeHandler.SetActive(true);
             FindObjectOfType<fadeTransition>().FadeToScene("NightPhase");
         }
     }
